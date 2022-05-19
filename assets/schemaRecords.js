a781c88e6a218d88f778498ebd8c625d2f25f9f1
Viviane Brenner <brenner.vivi@gmail.com>
2022-05-19 18:05:41 -0300

    Preparando revisão de código

--- assets/schemaRecords.js
+++
@@ -1,8 +0,0 @@
-import joi from 'joi'
-
-const schemaRecords = joi.object({
-    value: joi.number().required(),
-    description: joi.string().max(30).required()
-})
-
-export default schemaRecords;
